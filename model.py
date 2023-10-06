833a8c7b23e649e5aee32e40fabba7ea5b9841b1
jackhoang99 <114491294+jackhoang99@users.noreply.github.com>
2023-10-05 20:27:19 -0700

    updated

--- model.py
+++ model.py
@@ -19,7 +19,7 @@ Question: {question}
 
 Helpful answer:
 """
-modelrp=st.secrets['modelrp']
+modelrp="meta/llama-2-13b-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5"
 
 def load_llm():
     return Replicate(
